94c2ba9395dd4368fb8f619c4af25c2b29a25bf3
ayushborage28 <ayushborage28@gmail.com>
2023-08-10 18:58:39 +0530

    Auth pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,32 @@
 import styles from "./styles";
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import Topbar from "./scenes/globals/Topbar"
+import Topbar from "./scenes/globals/Topbar";
 import HomeScreen from "./scenes/HomeScreen";
+import { Route, Routes } from "react-router-dom";
+import Login from "./scenes/Login";
+import Signup from "./scenes/Signup";
 
 const App = () => {
   const [theme, colorMode] = useMode();
   return (
-    <div className="w-full  overflow-hidden">
-    <HomeScreen/>
-    
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {/* reset the css to default */}
+        <div className="app">
+          <main className="content">
+            <Topbar />
+            {/* <Routes>
+              <Route path="/" element={<HomeScreen />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+            </Routes> */}
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
-}
+};
 
 export default App;

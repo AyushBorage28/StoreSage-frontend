ef38b4e7e5cedf644da6a8ff86f29946956a05e3
Ishpreet-Kaur-Bedi <ishpreetshriya@gmail.com>
2023-08-11 18:33:33 +0530

    form added

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,11 @@ import { ColorModeContext, useMode } from "./theme";
 import Topbar from "./scenes/globals/Topbar";
 
 import Dashboard from "./scenes/dashboard";
-// import Team from "./scenes/team";
-// import Invoices from "./scenes/invoices";
-// import Contacts from "./scenes/contacts";
+import Team from "./scenes/team";
+import Invoices from "./scenes/invoices";
+import Contacts from "./scenes/contacts"
 // import Bar from "./scenes/bar";
-// import Form from "./scenes/form";
+import Form from "./scenes/forms";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";
 // import FAQ from "./scenes/faq";
@@ -42,10 +42,14 @@ const App = () => {
               <Route path="/signup" element={<Signup />} /> */}
 
               <Route path="/" element={<Dashboard />} />
-              {/* <Route path="/team" element={<Team />} />
+               <Route path="/team" element={<Team />} />
+              
               <Route path="/contacts" element={<Contacts />} />
+              
               <Route path="/invoices" element={<Invoices />} />
+              
               <Route path="/form" element={<Form />} />
+               {/*
               <Route path="/bar" element={<Bar />} />
               <Route path="/pie" element={<Pie />} />
               <Route path="/line" element={<Line />} />

3aa2a25b87c990145cebb9de8bdb416806bbd23f
ayushborage28 <ayushborage28@gmail.com>
2023-08-13 00:49:13 +0530

    add rest scenes

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Topbar from "./scenes/globals/Topbar";
 import Dashboard from "./scenes/dashboard";
 import Team from "./scenes/team";
 import Invoices from "./scenes/invoices";
-import Contacts from "./scenes/contacts"
+import Contacts from "./scenes/contacts";
 import Bar from "./scenes/bar";
 import Form from "./scenes/forms";
 import Line from "./scenes/line";
@@ -16,7 +16,7 @@ import FAQ from "./scenes/faq";
 import Geography from "./scenes/geography";
 import Sidebar from "./scenes/globals/Sidebar";
 
-// import Calendar from "./scenes/calendar";
+import Calendar from "./scenes/calendar";
 
 import HomeScreen from "./scenes/HomeScreen";
 import Login from "./scenes/Login";
@@ -42,26 +42,20 @@ const App = () => {
               <Route path="/signup" element={<Signup />} /> */}
 
               <Route path="/" element={<Dashboard />} />
-               <Route path="/team" element={<Team />} />
-              
+              <Route path="/team" element={<Team />} />
+
               <Route path="/contacts" element={<Contacts />} />
-              
+
               <Route path="/invoices" element={<Invoices />} />
-              
+
               <Route path="/form" element={<Form />} />
               <Route path="/faq" element={<FAQ />} />
               <Route path="/bar" element={<Bar />} />
               <Route path="/pie" element={<Pie />} />
-   
-              <Route path="/line" element={<Line />} />
-              {/* <Route path="/calendar" element={<Calendar />} /> */}
 
-              
-               
-             
-          
-            
-            
+              <Route path="/line" element={<Line />} />
+              <Route path="/calendar" element={<Calendar />} />
+
               <Route path="/geography" element={<Geography />} />
             </Routes>
           </main>

2d4c8234f3729ef1d6c5511dcfeb033751a03ea3
Ishpreet-Kaur-Bedi <ishpreetshriya@gmail.com>
2023-08-13 14:06:19 +0530

    fixed Bugs

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,19 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
 import Topbar from "./scenes/globals/Topbar";
 
-// import Dashboard from "./scenes/dashboard";
-// import Team from "./scenes/team";
-// import Invoices from "./scenes/invoices";
-// import Contacts from "./scenes/contacts";
-// import Bar from "./scenes/bar";
-// import Form from "./scenes/forms";
-// import Line from "./scenes/line";
-// import Pie from "./scenes/pie";
-// import FAQ from "./scenes/faq";
-// import Geography from "./scenes/geography";
-// import Sidebar from "./scenes/globals/Sidebar";
+import Dashboard from "./scenes/dashboard";
+import Team from "./scenes/team";
+import Invoices from "./scenes/invoices";
+import Contacts from "./scenes/contacts";
+import Bar from "./scenes/bar";
+import Form from "./scenes/forms";
+import Line from "./scenes/line";
+import Pie from "./scenes/pie";
+import FAQ from "./scenes/faq";
+import Geography from "./scenes/geography";
+import Sidebar from "./scenes/globals/Sidebar";
 
-// import Calendar from "./scenes/calendar";
+import Calendar from "./scenes/calendar";
 
 import HomeScreen from "./scenes/HomeScreen";
 import Login from "./scenes/Login";
@@ -25,6 +25,23 @@ import Signup from "./scenes/Signup";
 const App = () => {
   const [theme, colorMode] = useMode();
   const [isSidebar, setIsSidebar] = useState(true);
+  const location = useLocation();
+  const allowedRoutes = [
+    "/dashboard",
+    "/team",
+    "/contacts",
+    "/invoices",
+    "/form",
+    "/faq",
+    "/bar",
+    "/pie",
+    "/line",
+    "/calendar",
+    "/geography",
+  ];
+  const shouldDisplaySidebarAndTopbar = allowedRoutes.includes(
+    location.pathname
+  );
 
   return (
     <ColorModeContext.Provider value={colorMode}>
@@ -32,36 +49,38 @@ const App = () => {
         <CssBaseline />
         {/* reset the css to default */}
         <div className="app">
-          <Sidebar isSidebar={isSidebar} />
+          {shouldDisplaySidebarAndTopbar && <Sidebar isSidebar={isSidebar} />}
           <main className="content">
-            <Topbar setIsSidebar={setIsSidebar} />
+            {shouldDisplaySidebarAndTopbar && (
+              <Topbar setIsSidebar={setIsSidebar} />
+            )}
 
             <Routes>
-              {/* <Route path="/" element={<HomeScreen />} />
+              <Route path="/" element={<HomeScreen />} />
               <Route path="/login" element={<Login />} />
-              <Route path="/signup" element={<Signup />} /> */}
+              <Route path="/signup" element={<Signup />} />
 
-              <Route path="/" element={<Dashboard />} />
+              <Route path="/dashboard" element={<Dashboard />} />
               <Route path="/team" element={<Team />} />
 
-    //           <Route path="/contacts" element={<Contacts />} />
+              <Route path="/contacts" element={<Contacts />} />
 
-    //           <Route path="/invoices" element={<Invoices />} />
+              <Route path="/invoices" element={<Invoices />} />
 
-    //           <Route path="/form" element={<Form />} />
-    //           <Route path="/faq" element={<FAQ />} />
-    //           <Route path="/bar" element={<Bar />} />
-    //           <Route path="/pie" element={<Pie />} />
+              <Route path="/form" element={<Form />} />
+              <Route path="/faq" element={<FAQ />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} />
 
-    //           <Route path="/line" element={<Line />} />
-    //           <Route path="/calendar" element={<Calendar />} />
+              <Route path="/line" element={<Line />} />
+              <Route path="/calendar" element={<Calendar />} />
 
-    //           <Route path="/geography" element={<Geography />} />
-    //         </Routes>
-    //       </main>
-    //     </div>
-    //   </ThemeProvider>
-    // </ColorModeContext.Provider>
+              <Route path="/geography" element={<Geography />} />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 };
 

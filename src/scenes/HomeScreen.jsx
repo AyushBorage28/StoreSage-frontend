607f6a7dec544c9a7284242eccfd0ec8b9bfb748
Ishpreet-Kaur-Bedi <ishpreetshriya@gmail.com>
2023-08-13 14:46:02 +0530

    footer added

--- src/scenes/HomeScreen.jsx
+++ src/scenes/HomeScreen.jsx
@@ -2,6 +2,7 @@ import { Navbar,Hero} from "../components"
 import Sales from "../components/Sales"
 import USP from "../components/USP"
 import Login from "./Login"
+import Footer from "../components/Footer"
 function HomeScreen() {
   return (
     <div className="bg-black w-full overflow-hidden">
@@ -9,6 +10,7 @@ function HomeScreen() {
       <Hero/>
       <Sales/>
      <USP/>
+     <Footer/>
     
 
     </div>
